074dad3d7966ff0b5b767567eaabf3e9273ac635
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-04-23 17:47:52 +0200

    fix(scripts): version getter for python dir

--- scripts/fluid_admin.sh
+++ scripts/fluid_admin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-html_file="${SHELLHISTORY_VENV}/lib/python3.5/site-packages/flask_admin/templates/bootstrap3/admin/base.html"
+html_file="${SHELLHISTORY_VENV}/lib/python$(python -V | cut -d' ' -f2 | cut -d. -f-2)/site-packages/flask_admin/templates/bootstrap3/admin/base.html"
 sed -i 's/container/container-fluid/' "${html_file}"
 sed -i 's/navbar-default/navbar-default navbar-static-top/' "${html_file}"
